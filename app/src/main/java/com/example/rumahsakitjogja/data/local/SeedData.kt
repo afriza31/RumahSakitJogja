578163149752cdd479f42bf2f4f38abb10d52910
afriza31 <muhammadnaufalafriza@gmail.com>
2026-01-10 10:19:09 +0700

    Update by afriza's

--- app/src/main/java/com/example/rumahsakitjogja/data/local/SeedData.kt
+++ app/src/main/java/com/example/rumahsakitjogja/data/local/SeedData.kt
@@ -12,7 +12,7 @@ object SeedData {
             layanan = "UGD, Poli Anak, Poli Jantung, Farmasi",
             jamBuka = "24 Jam untuk UGD, Poli jam kerja",
             telepon = "0274-587333",
-            lat = -7.7689, lng = 110.3720,
+            lat = -7.767078677723353, lng = 110.37319526823036,
             logoResId = R.drawable.sardjito
         ),
         HospitalEntity(
@@ -23,7 +23,7 @@ object SeedData {
             layanan = "UGD, Poli Umum, Poli Penyakit Dalam",
             jamBuka = "24 Jam untuk UGD, Poli jam kerja",
             telepon = "0274-586688",
-            lat = -7.7829, lng = 110.3671,
+            lat = -7.783153361683199, lng = 110.37818659061413,
             logoResId = R.drawable.rs_bethesda
         ),
         HospitalEntity(
@@ -34,7 +34,7 @@ object SeedData {
             layanan = "UGD, Rawat Inap, Poli Umum, Lab",
             jamBuka = "24 Jam untuk UGD, Poli jam kerja",
             telepon = "0274-367381",
-            lat = -7.8878, lng = 110.3339,
+            lat = -7.891613394309161, lng = 110.33796153051127,
             logoResId = R.drawable.rsud_panembahan
         ),
         HospitalEntity(
@@ -45,7 +45,7 @@ object SeedData {
             layanan = "UGD, Poli Umum, Poli Anak, Radiologi",
             jamBuka = "24 Jam untuk UGD, Poli jam kerja",
             telepon = "0274-868405",
-            lat = -7.7169, lng = 110.3555,
+            lat = -7.680823873504729, lng = 110.34227681489658,
             logoResId = R.drawable.rsud_sleman
         ),
         HospitalEntity(
@@ -56,7 +56,7 @@ object SeedData {
             layanan = "UGD, Poli Umum, Rawat Inap",
             jamBuka = "24 Jam untuk UGD, Poli jam kerja",
             telepon = "0274-391012",
-            lat = -7.9675, lng = 110.6030,
+            lat = -7.956866050513851, lng = 110.6021941408069,
             logoResId = R.drawable.rsud_wonosari
         ),
         HospitalEntity(
@@ -67,7 +67,7 @@ object SeedData {
             layanan = "UGD, Poli Umum, Poli Anak",
             jamBuka = "24 Jam untuk UGD, Poli jam kerja",
             telepon = "0274-773169",
-            lat = -7.8587, lng = 110.1611,
+            lat = -7.860764990172421 , lng = 110.14649151268728,
             logoResId = R.drawable.rsud_wates
         ),
         HospitalEntity(
@@ -78,7 +78,7 @@ object SeedData {
             layanan = "Poli Jiwa, Rawat Inap Jiwa, Konsultasi Psikiatri",
             jamBuka = "Senin sampai Jumat jam kerja",
             telepon = "0274-439500",
-            lat = -7.7477, lng = 110.4038,
+            lat = -7.655969436435252, lng = 110.42269998609524,
             logoResId = R.drawable.grhasia
         ),
         HospitalEntity(
@@ -89,7 +89,7 @@ object SeedData {
             layanan = "Kebidanan, Anak, Imunisasi, USG",
             jamBuka = "Senin sampai Sabtu jam kerja",
             telepon = "0274-xxxxxx",
-            lat = -7.8015, lng = 110.3652,
+            lat = -7.767968167355577, lng = 110.41628782122089,
             logoResId = R.drawable.rskia_sadewa
         ),
         HospitalEntity(
